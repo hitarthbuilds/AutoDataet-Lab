e58a9822e8898f2aefd72f314b88029e0e168d95
hitarthbuilds <130482481+hitarthbuilds@users.noreply.github.com>
2025-11-25 17:21:47 +0530

    def fixed

--- core/eda/summary.py
+++ core/eda/summary.py
@@ -1,11 +1,15 @@
 import polars as pl
 
 def dataset_overview(df: pl.DataFrame) -> dict:
+
+    numeric_cols = [col for col, dt in zip(df.columns, df.dtypes) if dt in pl.NUMERIC_DTYPES]
+    categorical_cols = [col for col, dt in zip(df.columns, df.dtypes) if dt == pl.Utf8]
+
     return {
-        "Rows": df.height,                 # Polars property
-        "Columns": df.width,               # Polars property
+        "Rows": df.height,
+        "Columns": df.width,
         "Column Names": df.columns,
         "Dtypes": {col: str(dtype) for col, dtype in zip(df.columns, df.dtypes)},
-        "Numeric Columns": [col for col, dt in zip(df.columns, df.dtypes) if dt.is_numeric()],
-        "Categorical Columns": [col for col, dt in zip(df.columns, df.dtypes) if dt.is_utf8()],
+        "Numeric Columns": numeric_cols,
+        "Categorical Columns": categorical_cols
     }
