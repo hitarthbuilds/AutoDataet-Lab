a89d870adfc8e5afd4190610862784849af37a60
hitarthbuilds <130482481+hitarthbuilds@users.noreply.github.com>
2025-11-25 16:42:26 +0530

    tried skeleton

---
+++ app/main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AutoDataset Lab",
+    layout="wide"
+)
+
+st.title("AutoDataset Lab")
+
+st.write("Select a page from the sidebar to begin.")
